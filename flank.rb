671a2c53b8f99c8cbd2cf9b8998da3efd751d17b
Angus Mitchell <anguspmitchell@gmail.com>
2025-09-24 20:52:20 -0500

    update formula

--- flank.rb
+++ flank.rb
@@ -1,10 +1,15 @@
 class Flank < Formula
-  desc "Test formula that just sets up a data directory"
+  desc "Test formula that installs the flank script"
+  homepage "https://github.com/btf-org/flank"
+  url "https://github.com/btf-org/flank/archive/refs/heads/main.tar.gz"
   version "0.1.0"
-  url "file:///dev/null"
+  sha256 "<REPLACE_WITH_TARBALL_SHA256>"
+
 
 
   def install
+    bin.install "flank" => "flank" if File.exist?("flank")
+
     (prefix/"INSTALL_LOG.txt").write("Installed at #{Time.now}")
   end
 
