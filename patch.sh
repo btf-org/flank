5374204a7bac629fc1c2a3cefe49ce9af832ace7
Angus Mitchell <anguspmitchell@gmail.com>
2025-10-03 14:17:49 -0500

    patch helper

---
+++ patch.sh
@@ -0,0 +1,24 @@
+set -e
+
+if [ -z "$1" ]; then
+    echo "Must supply message"
+    exit 1
+fi
+
+# Get the latest tag matching vM.m.p
+latest_tag=$(git tag --list "v*.*.*" --sort=-v:refname | head -n1)
+
+if [ -z "$latest_tag" ]; then
+    echo "No tags found. Exiting..."
+    exit 1
+else
+
+version=${latest_tag#v}
+IFS='.' read -r major minor patch <<< "$version"
+
+# Increment patch
+patch=$((patch + 1))
+
+new_tag="v$major.$minor.$patch"
+
+git tag -a "$new_tag" -m $1
