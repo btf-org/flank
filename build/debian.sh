f02ae675266b0a1649b1e51bf766a58f00cc2f92
Angus Mitchell <anguspmitchell@gmail.com>
2025-10-15 11:50:50 -0500

    update path

--- build/debian.sh
+++ build/debian.sh
@@ -2,16 +2,16 @@ latest_tag=$(git tag --list "v*.*.*" --sort=-v:refname | head -n1)
 version=${latest_tag#v}
 echo "Latest tag is $version"
 read -rp "Hit [enter] to continue"
-mkdir -p releases/flank"$version"_amd64/DEBIAN
-mkdir -p releases/flank"$version"_amd64/usr/local/bin
-mkdir -p releases/flank"$version"_amd64/usr/share/flank
-mkdir -p releases/flank"$version"_amd64/var/lib/flank
+mkdir -p releases/flank_"$version"_amd64/DEBIAN
+mkdir -p releases/flank_"$version"_amd64/usr/local/bin
+mkdir -p releases/flank_"$version"_amd64/usr/share/flank
+mkdir -p releases/flank_"$version"_amd64/var/lib/flank
 
-cp build/DEBIAN/control build/DEBIAN/postinst releases/flank"$version"_amd64/DEBIAN/
-chmod 775 releases/flank"$version"_amd64/DEBIAN/postinst
-cp flank iflank releases/flank"$version"_amd64/usr/local/bin/
-docker run --rm -v "$PWD":/work -w /work ubuntu bash -c "apt update && apt install -y gcc && gcc flankserver.c -o releases/flank"$version"_amd64/usr/local/bin/flankserver"
-cp index.html releases/flank"$version"_amd64/usr/share/flank/
+cp build/DEBIAN/control build/DEBIAN/postinst releases/flank_"$version"_amd64/DEBIAN/
+chmod 775 releases/flank_"$version"_amd64/DEBIAN/postinst
+cp flank iflank releases/flank_"$version"_amd64/usr/local/bin/
+docker run --rm -v "$PWD":/work -w /work ubuntu bash -c "apt update && apt install -y gcc && gcc flankserver.c -o releases/flank_"$version"_amd64/usr/local/bin/flankserver"
+cp index.html releases/flank_"$version"_amd64/usr/share/flank/
 
-docker run --rm -v "$PWD":/work -w /work ubuntu bash -c "apt update && apt install -y tree && tree && pwd && dpkg-deb --build releases/flank"$version"_amd64"
+docker run --rm -v "$PWD":/work -w /work ubuntu bash -c "apt update && apt install -y tree && tree && pwd && dpkg-deb --build releases/flank_"$version"_amd64"
 
