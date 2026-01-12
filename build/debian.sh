3ca07c9750c9229e354d74f4449f95de67685836
Angus Mitchell <anguspmitchell@gmail.com>
2026-01-12 16:39:05 -0600

    update to v0.1.59

--- build/debian.sh
+++ build/debian.sh
@@ -11,7 +11,7 @@ mkdir -p releases/flank_"$version"_amd64/etc/default
 
 cp build/DEBIAN/control build/DEBIAN/postinst build/DEBIAN/conffiles releases/flank_"$version"_amd64/DEBIAN/
 cp build/DEBIAN/flankserver.service releases/flank_"$version"_amd64/lib/systemd/system
-cp build/DEBIAN/flank.conf releases/flank_"$version"_amd64/etc/default/flank.conf
+cp build/DEBIAN/flank releases/flank_"$version"_amd64/etc/default/flank
 
 sed -i '' 's/Version: .*$/Version: '$version'/' releases/flank_"$version"_amd64/DEBIAN/control
 chmod 775 releases/flank_"$version"_amd64/DEBIAN/postinst
