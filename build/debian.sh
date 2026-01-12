a9583a9147bd4cb10388c4bfef05eec5860da4b3
Angus Mitchell <anguspmitchell@gmail.com>
2026-01-12 16:32:06 -0600

    add conffiles and flank.conf to debian build

--- build/debian.sh
+++ build/debian.sh
@@ -7,9 +7,12 @@ mkdir -p releases/flank_"$version"_amd64/usr/local/bin
 mkdir -p releases/flank_"$version"_amd64/usr/share/flank
 mkdir -p releases/flank_"$version"_amd64/var/lib/flank
 mkdir -p releases/flank_"$version"_amd64/lib/systemd/system
+mkdir -p releases/flank_"$version"_amd64/etc/default
 
-cp build/DEBIAN/control build/DEBIAN/postinst releases/flank_"$version"_amd64/DEBIAN/
+cp build/DEBIAN/control build/DEBIAN/postinst build/DEBIAN/conffiles releases/flank_"$version"_amd64/DEBIAN/
 cp build/DEBIAN/flankserver.service releases/flank_"$version"_amd64/lib/systemd/system
+cp build/DEBIAN/flank.conf releases/flank_"$version"_amd64/etc/default/flank.conf
+
 sed -i '' 's/Version: .*$/Version: '$version'/' releases/flank_"$version"_amd64/DEBIAN/control
 chmod 775 releases/flank_"$version"_amd64/DEBIAN/postinst
 cp flank iflank releases/flank_"$version"_amd64/usr/local/bin/
