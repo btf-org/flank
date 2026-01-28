b9ed485d0d3a97044d35d6e341a5c2ebe8498f01
Angus Mitchell <anguspmitchell@gmail.com>
2026-01-28 12:20:12 -0600

    new quickstart

--- build/var/flank/Quickstarts/hello_world/template.sh
+++ build/var/flank/Quickstarts/hello_world/template.sh
@@ -1,4 +1,7 @@
-# @description Click on the Edit button to see the command that runs, plus the annotations that format this very page.
-# ${your_name} @colspan 2
+# @description First click "Run" to see the output.<br>Then click on "hello_world" on the top bar to come back to this page.<br>Then click on the "Edit" button on the bottom bar to parameterize this command.
  
-echo 'hello ${your_name}'
+# How to Parameterize:
+# Change "world" to "${name}" and click Save below. 
+# You should see an input box for "name" when you run the command
+
+echo 'hello, world'
