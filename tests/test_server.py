5442193a9881dc623d6225308b70b55a9ba63227
Angus Mitchell <anguspmitchell@gmail.com>
2026-08-28 13:50:27 -0500

    get the test working

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,16 +7,18 @@ def test_serves_index():
     server = subprocess.Popen([
         "./flank_server",
         "--iflank-path", "/bin/cat",
+        "--port", "8084",
     ])
 
     try:
         # Give flankserver a moment to start listening
         time.sleep(0.1)
 
-        response = urllib.request.urlopen("http://localhost:8083/")
-
+        response = urllib.request.urlopen("http://localhost:8084/")
         assert response.status == 200
-        assert b"<!DOCTYPE html>" in response.read()
+        body = response.read()
+        # any string that is unique to index.html
+        assert b"accumulatingResponse" in body
 
     finally:
         server.terminate()
